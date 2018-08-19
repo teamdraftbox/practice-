bf5794281b47b257be9e0991aedf2a0bec2ddd94
teamdraftbox <teamdraftbox@gmail.com>
2018-08-19 08:06:13 +0000

    intitial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require("express"),
+app = express()
+app.set("view engine","ejs")
+
+app.get("/",function(req,res){
+    res.render("home")
+})
+app.listen(process.env.PORT,process.env.IP)
