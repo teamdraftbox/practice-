2fc70a2de5ab7a60e0c0d27cc586d3ccb4e66671
teamdraftbox <teamdraftbox@gmail.com>
2018-08-19 13:41:48 +0000

    second route

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.set("view engine","ejs")
 app.get("/",function(req,res){
     res.render("home")
 })
+
+app.get("/app",function(req,res){
+    res.render("app")
+})
 app.listen(process.env.PORT,process.env.IP)
